285480b382224c38e0edd24ececd781df8f04559
s110103 <lukasschauer2704@gmail.com>
2021-01-09 15:10:49 +0100

    Initial Commit

---
+++ SautrogSystem-MacOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SautrogSystem-MacOS
+//
+//  Created by Lukas Schauer on 09.01.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
